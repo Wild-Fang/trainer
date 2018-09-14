ddc6bc0ca35f7f12c5b714ccd9ceaf26f3f70ac7
Arthur Guiot <arguiot@gmail.com>
2018-09-14 19:44:46 +0800

    Added trainer.swift

---
+++ Trainer.swift
@@ -0,0 +1,20 @@
+#!/usr/bin/swift
+// CreateML trainer
+
+import CreateML
+import Foundation
+
+// Specify where is the data
+let path = NSString(string: "~/dataset/").expandingTildeInPath
+let trainingDir = URL(fileURLWithPath: path).appendingPathComponent("training")
+let testDir = URL(fileURLWithPath: path).appendingPathComponent("test")
+
+// Create a mode
+let model = try MLImageClassifier(trainingData: .labeledDirectories(at: trainingDir))
+
+// Test the model
+let evaluation = model.evaluation(on: .labeledDirectories(at: testDir))
+
+// Save the model
+let home = NSString(string: "~").expandingTildeInPath
+try model.write(to: URL(fileURLWithPath: home).appendingPathComponent("AnimalClassifier.mlmodel"))
